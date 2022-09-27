4de5e0b9f5c8815ff601d5402961f6e633ade4c6
Kyrylo Dehtiarenko <k.degtyarenko@gmail.com>
2022-09-27 13:49:34 +0300

    adds tweets parser

--- main.py
+++ main.py
@@ -24,21 +24,45 @@ try:
 except Exception as err:
     print(f'Failed authentication: {err}')
 
-# users = api.lookup_users(screen_name=["homepancakeua"])
+# users = api.lookup_users(screen_name=["blabla"])
 # user = users[0]
 # print(user)
 # user = api.get_user(user_id=user.id)
 # print(user)
-tweets = api.user_timeline(screen_name="bmwant", count=10, exclude_replies=True)
+tweets = api.user_timeline(screen_name="blabla", count=20, exclude_replies=True, include_rts=True)
+# tweets = api.user_timeline(screen_name="blabla", count=10, exclude_replies=True, truncated=False)
 # print(tweets)
 print(len(tweets))
 
 for tweet in tweets:
-    print(tweet.id)
-    print(tweet.text)
-    print(tweet.created_at)
-    print(tweet.in_reply_to_screen_name)
-    print(tweet.author.id)
-    print(tweet.user.id)
-    print(tweet._json)
-    break
+    video_urls = []
+    photo_urls = []
+
+    if hasattr(tweet, "extended_entities"):
+        for m in tweet.extended_entities.get("media", []):
+            match media_type := m.get("type"):
+                case "video":
+                    video_urls = [v["url"] for v in m.get("video_info", {}).get("variants", [])]
+                case "photo":
+                    photo_urls = [v["url"] for v in m.get("video_info", {}).get("variants", [])]
+
+    if is_quote_status := hasattr(tweet, "quoted_status"):
+        quoted_author = tweet.quoted_status
+
+    print({
+        "id": tweet.id,
+        "text": tweet.text,
+        "link": f"https://twitter.com/i/web/status/{tweet.id}",
+        "author_id": quoted_author.id if is_quote_status else tweet.user.id,
+        "author_name": quoted_author.user.screen_name if is_quote_status else tweet.user.screen_name,
+        "reply_to_id": tweet.in_reply_to_user_id or "",
+        "reply_to_name": tweet.in_reply_to_screen_name or "",
+        "owner_id": tweet.user.id,
+        "owner_name": tweet.user.screen_name,
+        "published_at": tweet.created_at.isoformat(),
+        "images": photo_urls,
+        "videos": video_urls,
+        "forwards": tweet.retweet_count,
+        # "replies": replies_count if replies else 0,
+        "likes": tweet.favorite_count,
+    })
